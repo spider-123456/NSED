b215e49111a2b78ac7952b49715c4e83ec21dae4
spider-123456 <73099091+spider-123456@users.noreply.github.com>
2024-07-20 13:58:02 +0800

    Update data_set.py

--- data_set.py
+++ data_set.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @File  : data_set.py
-# @Author: yanms
-# @Date  : 2021/11/1 11:38
+# @Author: Wcai
+# @Date  : 2024/3/26 11:38
 # @Desc  :
 import argparse
 import os
